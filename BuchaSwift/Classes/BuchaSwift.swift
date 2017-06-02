fd2f4a12a5af91e779db555844547ac7dd8e30b4
truebucha <truebucha@gmail.com>
2017-06-02 15:03:54 +0300

    fix mistyping

--- BuchaSwift/Classes/BuchaSwift.swift
+++ BuchaSwift/Classes/BuchaSwift.swift
@@ -1,4 +1,4 @@
 
 public typealias Completion = () -> Void
 public typealias ErrorCompletion<ErrorType> = (_ error : ErrorType?) -> ()
-public typealias DataCompletion<ErrorType, DataType> = (_ data: DataType, _ error: ErrorType?) -> ()                                                 
+public typealias DataCompletion<DataType, ErrorType> = (_ data: DataType, _ error: ErrorType?) -> ()
